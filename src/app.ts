4ba5f154861aae0ecc87b9feb86996412d5dd5a6
ndktr <ndktr.git@gmail.com>
2023-10-18 17:28:47 +0900

    fix: update node server loading html file

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,64 @@
 import * as http from 'http';
+import * as fs from 'fs';
 
 const hostname: string = '127.0.0.1';
 const port: number = 3000;
 
-const server: http.Server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World');
+const returnAppropriateHtml = (url: undefined | string): string => {
+  if (url === undefined) return '/index.html';
+  const appropriateHtml: string = url === '/' ? '/index.html' : `${url}.html`;
+  return appropriateHtml;
+};
+
+const returnPathToHtml = (htmlFileName: string): string => {
+  const pathToHtml = `./dist/html${htmlFileName}`;
+  return pathToHtml;
+};
+
+const isFaviconReq = (url: string | undefined): boolean => {
+  if (url === '/favicon.ico') return true;
+  return false;
+};
+
+const scribbleLog = (text: any): void => {
+  // eslint-disable-next-line no-console
+  console.log(text);
+};
+
+const server: http.Server = http.createServer((req, res): void => {
+  const reqUrl: string | undefined = req.url;
+
+  if (reqUrl === undefined) {
+    res.writeHead(500);
+    res.end('Something Error occured');
+  }
+
+  if (isFaviconReq(reqUrl)) {
+    res.writeHead(200, { 'Content-Type': 'image/x-icon' });
+  }
+
+  if (!isFaviconReq(reqUrl)) {
+    if (reqUrl?.includes('.js')) {
+      const pathToJS: string = `./dist${reqUrl}`;
+      fs.readFile(pathToJS, (error, content): void => {
+        if (!error) {
+          res.writeHead(200, { 'Content-Type': 'text/javascript' });
+          res.end(content, 'utf-8');
+        }
+      });
+    } else {
+      const htmlFileName: string = returnAppropriateHtml(reqUrl);
+      const pathToHtml: string = returnPathToHtml(htmlFileName);
+      fs.readFile(pathToHtml, (error, content): void => {
+        if (!error) {
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.end(content, 'utf-8');
+        }
+      });
+    }
+  }
 });
 
-server.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, hostname, (): void => {
+  scribbleLog(`Server running at http://${hostname}:${port}/`);
 });
--- src/frontend/index.ts
+++ src/frontend/index.ts
@@ -2,5 +2,5 @@ const frontendFunc = () => {
   const message: string = 'This is my first message at frontend';
   return message;
 };
-
+console.log('Hello World');
 export default frontendFunc;
