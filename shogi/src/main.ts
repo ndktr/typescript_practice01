c0ba065fddd00c100402ce7178258e53f70bea75
ndktr <ndktr.git@gmail.com>
2023-12-20 17:23:00 +0900

    add: implement function show piece's next positions

--- shogi/src/main.ts
+++ shogi/src/main.ts
@@ -1,34 +1,56 @@
 import './style.css'
 import { Board } from './models/Board.ts'
-import { Piece, Rook, Bishop, Gold, Pawn } from './models/Piece.ts'
+import { Piece, Rook, Bishop, Gold, Silver, Knight, Lance, Pawn } from './models/Piece.ts'
 
 // Piece Test
 const pawn: Piece = new Pawn('P', false, [6, 0])
-const possiblePositions1: number[][] = pawn.getPossiblePositions()
-console.log('possiblePosition1: [6, 0]')
+const possiblePositions1: number[][] = pawn.getNextPositions()
+console.log('pawn: possiblePosition1: [6, 0]')
 console.log(possiblePositions1)
 
 // Piece Test
-const gold: Piece = new Gold('G', false, [8, 3])
-const possiblePositions2: number[][] = gold.getPossiblePositions()
-console.log('possiblePosition2: [8, 3]')
+const lance: Piece = new Lance('L', true, [0, 8])
+const possiblePositions2: number[][] = lance.getNextPositions()
+console.log('lance: possiblePosition2: [0, 8]')
 console.log(possiblePositions2)
 
 // Piece Test
-const rook: Piece = new Rook('R', true, [1, 1])
-const possiblePosition3: number[][] = rook.getPossiblePositions()
-console.log('possiblePosition3: [1, 1]')
-console.log(possiblePosition3)
+const knight: Piece = new Knight('K', false, [8, 7])
+const possiblePositions3: number[][] = knight.getNextPositions()
+console.log('knight: possiblePosition3: [8, 7]')
+console.log(possiblePositions3)
 
-// Piece Test
-const bishop: Piece = new Bishop('B', false, [7, 1])
-const possiblePosition4: number[][] = bishop.getPossiblePositions()
-console.log('possiblePosition4: [7, 1]')
+// // Piece Test
+const silver: Piece = new Silver('S', true, [0, 2])
+const possiblePosition4: number[][] = silver.getNextPositions()
+console.log('silver: possiblePosition5: [0, 2]')
 console.log(possiblePosition4)
+
+// // Piece Test
+const gold: Piece = new Gold('G', false, [8, 3])
+const possiblePositions5: number[][] = gold.getNextPositions()
+console.log('gold: possiblePosition2: [8, 3]')
+console.log(possiblePositions5)
+
+// // Piece Test
+const rook: Piece = new Rook('R', true, [1, 1])
+const possiblePosition6: number[][] = rook.getNextPositions()
+console.log('rook: possiblePosition3: [1, 1]')
+console.log(possiblePosition6)
+
+// // Piece Test
+const bishop: Piece = new Bishop('B', false, [7, 7])
+const possiblePosition7: number[][] = bishop.getNextPositions()
+console.log('bishop: possiblePosition4: [7, 7]')
+console.log(possiblePosition7)
 
 // Board Test
 const board: Board = new Board()
+pawn.getCurrentPosition()
 board.set(6, 0, pawn)
+board.set(0, 8, lance)
+board.set(8, 7, knight)
+board.set(0, 2, silver)
 board.set(8, 3, gold)
 board.set(1, 1, rook)
 board.set(7, 1, bishop)
--- shogi/src/models/Piece.ts
+++ shogi/src/models/Piece.ts
@@ -1,3 +1,6 @@
+import { PieceCalcurator } from "../utils/PieceCalcurator.ts"
+
+
 export class Piece {
   private name: string
   private isForward: boolean
@@ -11,57 +14,85 @@ export class Piece {
     this.steps = steps
   }
   
-  public getPossiblePositions(): number[][] {
-    const row: number = this.current[0]
-    const column: number = this.current[1]
-    const possiblePositions = this.calcPossiblePositions(row, column)
-    return possiblePositions
+  public getNextPositions(): number[][] {
+    const forward: number[] = (
+      PieceCalcurator.getNextPosition(
+        'forward', this.isForward, this.current, this.steps[0]))
+    const rightForward: number[] = (
+      PieceCalcurator.getNextPosition(
+        'rightForward', this.isForward, this.current, this.steps[1]))
+    const right: number[] = (
+      PieceCalcurator.getNextPosition(
+        'right', this.isForward, this.current, this.steps[2]))
+    const rightBackward: number[] = (
+      PieceCalcurator.getNextPosition(
+        'rightBackward', this.isForward, this.current, this.steps[3]))
+    const backward: number[] = (
+      PieceCalcurator.getNextPosition(
+        'backward', this.isForward, this.current, this.steps[4]))
+    const leftBackward: number[] = (
+      PieceCalcurator.getNextPosition(
+        'leftBackward', this.isForward, this.current, this.steps[5]))
+    const left: number[] = (
+      PieceCalcurator.getNextPosition(
+        'left', this.isForward, this.current, this.steps[6]))
+    const leftForward: number[] = (
+      PieceCalcurator.getNextPosition(
+        'leftForward', this.isForward, this.current, this.steps[7]))
+    const knightRightForward: number[] = (
+      PieceCalcurator.getNextPosition(
+        'knightRightForward', this.isForward, this.current, this.steps[8]))
+    const knightLeftForward: number[] = (
+      PieceCalcurator.getNextPosition(
+        'knightLeftForward', this.isForward, this.current, this.steps[9]))
+      return [
+        forward, rightForward, right, rightBackward, backward, leftBackward,
+        left, leftForward, knightRightForward, knightLeftForward]
   }
 
-  public calcPossiblePositions(row: number, column: number): number[][] {
-      const forward: number[] = this.calcPossibleForward(row, column, this.steps[0], this.isForward)
-      const rightForward: number[] = this.isForward ? [row+this.steps[1], column-this.steps[1]] : [row-this.steps[1], column+this.steps[1]] 
-      const right: number[] = this.isForward ? [row, column-this.steps[2]] : [row, column+this.steps[2]]
-      const rightBackward: number[] = this.isForward ? [row-this.steps[3], column-this.steps[3]] : [row+this.steps[3], column+this.steps[3]]
-      const backword: number[] = this.isForward ? [row-this.steps[4], column] : [row+this.steps[4], column]
-      const leftBackword: number[] = this.isForward ? [row-this.steps[5], column+this.steps[5]] : [row+this.steps[5], column-this.steps[5]]
-      const left: number[] = this.isForward ? [row, column+this.steps[6]] : [row, column-this.steps[6]]
-      const leftForward: number[] = this.isForward ? [row+this.steps[7], column+this.steps[7]] : [row-this.steps[7], column-this.steps[7]]
-      const knightRightForward: number[] = this.isForward ? [row+(this.steps[8]*2), column-this.steps[8]] : [row-(this.steps[8]*2), column+this.steps[8]]
-      const knightLeftForward: number[] = this.isForward ? [row+(this.steps[9]*2), column+this.steps[9]] : [row-(this.steps[9]*2), column-this.steps[9]]
-      return [forward, rightForward, right, rightBackward, backword, leftBackword, left, leftForward, knightRightForward, knightLeftForward]
-  }
-
-  private calcPossibleForward(row: number, column: number, forwardStep: number, isForward: boolean): number[] {
-    if (isForward) return (row + forwardStep > 8) ? [8, column] : [row+forwardStep, column]
-    return (row - forwardStep < 0) ? [0, column] : [row-forwardStep, column]
-  }
-
-  private calcPossibleRightForward(row: number, column: number, forwardStep: number, isForward: boolean): number {
-    //
+  public getCurrentPosition() {
+    return this.current
   }
 }
 
 export class Pawn extends Piece {
-  constructor(name: string, isForward: boolean, current: number[]) {
+  constructor (name: string, isForward: boolean, current: number[]) {
     super(name, isForward, current, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
   }
 }
 
 export class Gold extends Piece {
-  constructor(name: string, isForward: boolean, current: number[]) {
+  constructor (name: string, isForward: boolean, current: number[]) {
     super(name, isForward, current, [1, 1, 1, 0, 1, 0, 1, 1, 0, 0])
   }
 }
 
+export class Silver extends Piece {
+  constructor (name: string, isForward: boolean, current: number[]) {
+    super(name, isForward, current, [1, 1, 0, 1, 0, 1, 0, 1, 0, 0])
+  }
+}
+
+export class Knight extends Piece {
+  constructor (name: string, isForward: boolean, current: number[]) {
+    super(name, isForward, current, [0, 0, 0, 0, 0, 0, 0, 0, 1, 1])
+  }
+}
+
+export class Lance extends Piece {
+  constructor (name: string, isForward: boolean, current: number[]) {
+    super(name, isForward, current, [8, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+  }
+}
+
 export class Rook extends Piece {
-  constructor(name: string, isForward: boolean, current: number[]) {
+  constructor (name: string, isForward: boolean, current: number[]) {
     super(name, isForward, current, [8, 0, 8, 0, 8, 0, 8, 0, 0, 0])
   }
 }
 
 export class Bishop extends Piece {
-  constructor(name: string, isForward: boolean, current: number[]) {
+  constructor (name: string, isForward: boolean, current: number[]) {
     super(name, isForward, current, [0, 8, 0, 8, 0, 8, 0, 8, 0, 0])
   }
 }
