e95fe964b0858d4ec3c309f48e644697ba4209d1
ndktr <ndktr.git@gmail.com>
2024-01-18 23:10:39 +0900

    fix: change piece model structure

--- src/models/Piece.ts
+++ src/models/Piece.ts
@@ -1,69 +1,87 @@
+import { Front, Back } from '../types/piece' 
+
 import { cloneDeep } from 'lodash'
 
 import PieceDirectionManager from '../services/PieceDirectionManager.ts'
 
-
 export class Piece {
-  private name: string
+  private front: Front 
+  private back: Back|null 
   private belongTo: number 
   private isForward: boolean
   private current: number[]
   private previous: number[]
-  private steps: number[]
   private onBoard: boolean
-  private promoteTo: string
+  private promoted: boolean
 
-  constructor(
-    name: string, belongTo: number,isForward: boolean, current: number[],
-    steps: number[], promoteTo: string) {
-    this.name = name
+  constructor(front: Front, back: Back|null, belongTo: number, isForward: boolean,
+    current: number[]) {
+    this.front = front
+    this.back = back
     this.belongTo = belongTo
     this.isForward = isForward
     this.current = current 
     this.previous = current 
-    this.steps = steps
     this.onBoard = true
-    this.promoteTo = promoteTo 
+    this.promoted = false 
   }
   
   public getAllNextPositions(): number[][][] {
+    const steps: number[] = this.getSteps() 
+
     const forward: number[][] = (
       PieceDirectionManager.getNextPositions(
-        'forward', this.isForward, this.current, this.steps[0]))
+        'forward', this.isForward, this.current, steps[0]))
+
     const rightForward: number[][] = (
       PieceDirectionManager.getNextPositions(
-        'rightForward', this.isForward, this.current, this.steps[1]))
+        'rightForward', this.isForward, this.current, steps[1]))
+
     const right: number[][] = (
       PieceDirectionManager.getNextPositions(
-        'right', this.isForward, this.current, this.steps[2]))
+        'right', this.isForward, this.current, steps[2]))
+
     const rightBackward: number[][] = (
       PieceDirectionManager.getNextPositions(
-        'rightBackward', this.isForward, this.current, this.steps[3]))
+        'rightBackward', this.isForward, this.current, steps[3]))
+
     const backward: number[][] = (
       PieceDirectionManager.getNextPositions(
-        'backward', this.isForward, this.current, this.steps[4]))
+        'backward', this.isForward, this.current, steps[4]))
+
     const leftBackward: number[][] = (
       PieceDirectionManager.getNextPositions(
-        'leftBackward', this.isForward, this.current, this.steps[5]))
+        'leftBackward', this.isForward, this.current, steps[5]))
+
     const left: number[][] = (
       PieceDirectionManager.getNextPositions(
-        'left', this.isForward, this.current, this.steps[6]))
+        'left', this.isForward, this.current, steps[6]))
+
     const leftForward: number[][] = (
       PieceDirectionManager.getNextPositions(
-        'leftForward', this.isForward, this.current, this.steps[7]))
+        'leftForward', this.isForward, this.current, steps[7]))
+
     const knightRightForward: number[][] = (
       PieceDirectionManager.getNextPositions(
-        'knightRightForward', this.isForward, this.current, this.steps[8]))
+        'knightRightForward', this.isForward, this.current, steps[8]))
+
     const knightLeftForward: number[][] = (
       PieceDirectionManager.getNextPositions(
-        'knightLeftForward', this.isForward, this.current, this.steps[9]))
+        'knightLeftForward', this.isForward, this.current, steps[9]))
+
     return [
       forward, rightForward, right, rightBackward, backward, leftBackward,
       left, leftForward, knightRightForward, knightLeftForward]
   }
 
   public getName() {
-    return this.name
+    if (this.promoted && this.back !== null) return this.back.name 
+    return this.front.name
+  }
+  
+  public getSteps() {
+    if (this.promoted && this.back !== null) return this.back.steps 
+    return this.front.steps
   }
 
   public getCurrentPosition() {
@@ -106,91 +124,82 @@ export class Piece {
     this.onBoard = false
   }
 
-  public getPromoteTo() {
-    return this.promoteTo
+  public promote() {
+    this.promoted = true
+  }
+
+  public isPromoted() {
+    return this.promoted
+  }
+
+  public init() {
+    this.promoted = false
+    this.onBoard = false
+    this.current = [-1, -1]
+    this.previous = [-1, -1]
   }
 }
 
 export class Pawn extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [1, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'SuperPawn')
-  }
-}
-
-export class SuperPawn extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [1, 1, 1, 0, 1, 0, 1, 1, 0, 0], '')
+  constructor (belongTo: number, isForward: boolean, current: number[]) {
+    const front = {name: '歩', steps: [1, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+    const back = {name: 'と', steps: [1, 1, 1, 0, 1, 0, 1, 1, 0, 0]}
+    super(front, back, belongTo, isForward, current)
   }
 }
 
 export class Lance extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [8, 0, 0, 0, 0, 0, 0, 0, 0, 0], 'SuperLance')
-  }
-}
-
-export class SuperLance extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [1, 1, 1, 0, 1, 0, 1, 1, 0, 0], '')
+  constructor (belongTo: number, isForward: boolean, current: number[]) {
+    const front = {name: '香', steps: [8, 0, 0, 0, 0, 0, 0, 0, 0, 0]}
+    const back = {name: '杏', steps: [1, 1, 1, 0, 1, 0, 1, 1, 0, 0]}
+    super(front, back, belongTo, isForward, current)
   }
 }
 
 export class Knight extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [0, 0, 0, 0, 0, 0, 0, 0, 1, 1], 'SuperKnight')
-  }
-}
-
-export class SuperKnight extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [1, 1, 1, 0, 1, 0, 1, 1, 0, 0], '')
+  constructor (belongTo: number, isForward: boolean, current: number[]) {
+    const front = {name: '桂', steps: [0, 0, 0, 0, 0, 0, 0, 0, 1, 1]}
+    const back = {name: '圭', steps: [1, 1, 1, 0, 1, 0, 1, 1, 0, 0]}
+    super(front, back, belongTo, isForward, current)
   }
 }
 
 export class Silver extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [1, 1, 0, 1, 0, 1, 0, 1, 0, 0], 'SuperSilver')
-  }
-}
-
-export class SuperSilver extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [1, 1, 1, 0, 1, 0, 1, 1, 0, 0], '')
+  constructor (belongTo: number, isForward: boolean, current: number[]) {
+    const front = {name: '銀', steps: [1, 1, 0, 1, 0, 1, 0, 1, 0, 0]}
+    const back = {name: '全', steps: [1, 1, 1, 0, 1, 0, 1, 1, 0, 0]}
+    super(front, back, belongTo, isForward, current)
   }
 }
 
 export class Gold extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [1, 1, 1, 0, 1, 0, 1, 1, 0, 0], '')
+  constructor (belongTo: number, isForward: boolean, current: number[]) {
+    const front = {name: '金', steps: [1, 1, 1, 0, 1, 0, 1, 1, 0, 0]}
+    const back = null
+    super(front, back, belongTo, isForward, current)
   }
 }
 
 export class Bishop extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [0, 8, 0, 8, 0, 8, 0, 8, 0, 0], 'SuperBishop')
-  }
-}
-
-export class SuperBishop extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [1, 8, 1, 8, 1, 8, 1, 8, 0, 0], '')
+  constructor (belongTo: number, isForward: boolean, current: number[]) {
+    const front = {name: '角', steps: [0, 8, 0, 8, 0, 8, 0, 8, 0, 0]}
+    const back = {name: '馬', steps: [1, 8, 1, 8, 1, 8, 1, 8, 0, 0]}
+    super(front, back, belongTo, isForward, current)
   }
 }
 
 export class Rook extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [8, 0, 8, 0, 8, 0, 8, 0, 0, 0], 'SuperRook')
-  }
-}
-
-export class SuperRook extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [8, 1, 8, 1, 8, 1, 8, 1, 0, 0], '')
+  constructor (belongTo: number, isForward: boolean, current: number[]) {
+    const front = {name: '飛', steps: [8, 0, 8, 0, 8, 0, 8, 0, 0, 0]}
+    const back = {name: '龍', steps: [8, 1, 8, 1, 8, 1, 8, 1, 0, 0]}
+    super(front, back, belongTo, isForward, current)
   }
 }
 
 export class King extends Piece {
-  constructor (name: string, belongTo: number, isForward: boolean, current: number[]) {
-    super(name, belongTo, isForward, current, [1, 1, 1, 1, 1, 1, 1, 1, 0, 0], '')
+  constructor (belongTo: number, isForward: boolean, current: number[]) {
+    const front = {name: '王', steps: [1, 1, 1, 1, 1, 1, 1, 1, 0, 0]}
+    const back = null
+    super(front, back, belongTo, isForward, current)
   }
 }
